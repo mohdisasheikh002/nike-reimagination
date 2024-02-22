098b93c2f9cd9f625115a744e925fa81f9f223f8
mohdisasheikh <mdishk002@gmail.com>
2024-02-22 16:47:44 +0530

    Changes in html and stylesheet

---
+++ script.js
@@ -0,0 +1,21 @@
+gsap.registerPlugin(ScrollTrigger);
+var counter = document.querySelector(".counter");
+
+gsap.to(counter, {
+  scrollTrigger: {
+    trigger: ".sec7",
+    start: "top 0%",
+  },
+  onUpdate: () => {
+    let flag = 0;
+
+    setInterval(() => {
+      if (flag == 500) {
+        clearInterval();
+      } else {
+        flag += 1;
+        counter.textContent = flag + " " + "+";
+      }
+    });
+  },
+});
